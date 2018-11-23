c9f76d824d213b4117a98ccd61ad194cfc3c150a
Aaron Aung <aaronaung.95@gmail.com>
2018-11-23 12:01:28 -0800

    Add loading feedback on featured projects load

--- src/app/components/featured-projects/featured-projects.component.ts
+++ src/app/components/featured-projects/featured-projects.component.ts
@@ -3,16 +3,20 @@ import { GlobalGivingService } from 'src/app/providers/globalgiving.service';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { DialogService } from 'src/app/providers/dialog.service';
+import { finalize } from 'rxjs/operators';
+import { fadeAnimation } from 'src/app/animations/router-transition';
 
 @Component({
   selector: 'app-featured-projects',
   templateUrl: './featured-projects.component.html',
-  styleUrls: ['./featured-projects.component.scss']
+  styleUrls: ['./featured-projects.component.scss'],
+  animations: [fadeAnimation]
 })
 export class FeaturedProjectsComponent implements OnInit {
   /** @todo - REMOVE SUMMARY FROM THE DISPLAY - TOO MUCH INFO - display stuff users actually care about (metrics) - summary could be on MoreInfo */
   featuredProjects: any;
   maxSummaryLength: number = 300;
+  loading: boolean = true;
   
   constructor(
     private globalGivingService: GlobalGivingService,
@@ -21,15 +25,19 @@ export class FeaturedProjectsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // this.toastr.info("You are currently viewing projects that are in need of urgent help.")
-    this.globalGivingService.getFeaturedProjects().subscribe({
-      next: (result) => {
-        this.featuredProjects = result;
-      },
-      error: () => {
-        this.toastr.error("There was an error in getting data from Global Giving. Please contact the developer.")
-      }
-    })
+    this.loading = true;
+    this.globalGivingService.getFeaturedProjects()
+      .pipe(finalize(() => {
+        this.loading = false;
+      }))
+      .subscribe({
+        next: (result) => {
+          this.featuredProjects = result;
+        },
+        error: () => {
+          this.toastr.error("There was an error in getting data from Global Giving. Please contact the developer.")
+        }
+      })
   }
 
   openDialog(project, dialog) {
